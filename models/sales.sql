5f8dfb8a181fdfc0b57814ba9702c48623eeee5a
Fahad Saeed <rrfwa71@gmail.com>
2023-10-18 10:49:32 +0000

    margin percent +product margin

--- models/sales.sql
+++ models/sales.sql
@@ -21,5 +21,7 @@ SELECT
 	,ROUND(s.quantity*CAST(p.purchSE_PRICE AS FLOAT64),2) AS purchase_cost
 	-- margin --
 	,s.revenue - s.quantity*CAST(p.purchSE_PRICE AS FLOAT64) AS margin
-FROM sales s
+    ,{{ margin_percent('s.revenue', 's.quantity*CAST(p.purchSE_PRICE AS FLOAT64)') }}AS margin_percent 
+    , {{ product_margin('s.revenue', 'CAST(p.purchSE_PRICE AS FLOAT64)*s.quantity','s.quantity') }} as product_margin
+    FROM sales s
 INNER JOIN product p ON s.pdt_id = p.products_id
